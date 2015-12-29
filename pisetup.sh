010d360aec505fa5ca98abb5a5748fe4ea577c0b
Chris Kernaghan <chris.kernaghan@gmail.com>
2015-12-29 01:33:21 +0000

    Useful scripts for Raspberry PI

---
+++ pisetup.sh
@@ -0,0 +1,7 @@
+Hostname="raspberrypib_1"
+IP_Address="192.168.100.51"
+
+sudo echo $IP_Address	$Hostname >>/etc/hosts
+sudo echo $Hostname >/etc/hostname
+sudo /etc/init.d/hostname.sh
+sudo reboot now
