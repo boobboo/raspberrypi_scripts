5df58fec88f5c048b78592fcfe8d58ace40afd87
Chris Kernaghan <chris.kernaghan@gmail.com>
2015-12-29 02:05:04 +0000

    Added RDP and update to the set up script

--- pisetup.sh
+++ pisetup.sh
@@ -4,4 +4,6 @@ IP_Address="192.168.100.51"
 sudo echo $IP_Address	$Hostname >>/etc/hosts
 sudo echo $Hostname >/etc/hostname
 sudo /etc/init.d/hostname.sh
+sudo apt-get update
+sudo apt-get install -y xrdp
 sudo reboot now
